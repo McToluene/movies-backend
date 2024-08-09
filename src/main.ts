6d04db3fef9e7d5de9bb6a8a4b7e00a1a75deb6c
mctoluene <xclusiveniggaz10@live.com>
2024-08-09 15:53:43 +0100

    Auth added to docs

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setTitle('Movies')
     .setDescription('The api documentation for movies app')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
